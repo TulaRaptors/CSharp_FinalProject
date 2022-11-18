8e4da3f71db3fe93c5a607834a64d81dd273cabe
Sergey_Podgornov <psv_23@mail.ru>
2022-11-18 17:51:19 +0300

    Функция Аккермана

---
+++ Ex_003_AckermannFunction/Program.cs
@@ -0,0 +1,19 @@
+﻿// Написать программу вычисления функции Аккермана
+
+int AckermannFunction(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else
+      if ((m != 0) && (n == 0))
+        return AckermannFunction(m - 1, 1);
+    else
+        return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+}
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine($"Функция Аккермана: А({m};{n}) = {AckermannFunction(m, n)}");
