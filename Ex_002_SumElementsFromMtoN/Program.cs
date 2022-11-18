0583914e454afe779f897c69fbd293470d63ef48
Sergey_Podgornov <psv_23@mail.ru>
2022-11-18 17:30:18 +0300

    Сумма элементов от M до N

---
+++ Ex_002_SumElementsFromMtoN/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+
+int SumElements(int m, int n)
+{
+    return m == n ? m : SumElements(m + 1, n) + m;
+}
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine($"Сумма элементов от {m} до {n}: {SumElements(m, n)}");
