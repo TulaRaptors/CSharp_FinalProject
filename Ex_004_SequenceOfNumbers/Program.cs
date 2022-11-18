7b1f0fe6b563deda3fe9f765a79eb5ebf9a2feed
Sergey_Podgornov <psv_23@mail.ru>
2022-11-18 18:39:35 +0300

    Последовательность чисел, где каждое следующее равно сумме предыдущих

---
+++ Ex_004_SequenceOfNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿// Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. 
+// Первые два элемента последовательности задаются пользователем
+
+void SequenceOfNumbers(int m, int n, int count)
+{
+    if (count != 0)
+    {
+        Console.Write($"{m + n} ");
+        SequenceOfNumbers(n, m + n, count - 1);
+    }
+}
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите второе число: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Последовательность чисел: ");
+Console.Write($"{m} {n} ");
+SequenceOfNumbers(m, n, 10);
