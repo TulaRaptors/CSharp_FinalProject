22357ff88c01125c5ef12092bc9942883824a9da
Sergey_Podgornov <psv_23@mail.ru>
2022-11-18 17:08:59 +0300

    Показать натуральные числа от M до N

---
+++ Ex_001_ShowNaturalNumbersFromMtoN/Program.cs
@@ -0,0 +1,18 @@
+﻿//Показать натуральные числа от M до N, N и M заданы
+
+void ShowNumbers(int m, int n)
+{
+    if (m <= n)
+    {
+        Console.Write(m + " ");
+        ShowNumbers(m + 1, n);
+    }
+}
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine($"Натуральные числа от {m} до {n}: ");
+ShowNumbers(m, n);
